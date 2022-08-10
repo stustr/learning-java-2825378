696ab1c898cb277fa830f2c64f43bc7b33bba58b
1919dev <sfbsturrock@gmail.com>
2022-08-10 12:22:16 +0100

    [first commit]

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static double calc_salary(double weeklyHours, double hourlyRate, int vacationDays) {
+        if (hourlyRate < 0) {
+            return -1;
+        }
         
+        if (weeklyHours < 0) {
+            return -1;
+        }
+        
+        double weeklyWage = weeklyHours * hourlyRate;
+        double vacationDeduction = vacationDays * hourlyRate * 8;
+        double salary = weeklyWage * 52 - vacationDeduction;
+        return salary;
     }
 
+    public static void main(String[] args) {
+        System.out.println(calc_salary(40, 10, 0));
+    }
 }
